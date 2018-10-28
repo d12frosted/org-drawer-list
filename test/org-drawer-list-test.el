5e7b8a81cb33e8ba3296f5b4dd5a9fde26ceedb6
Boris Buliga <boris@d12frosted.io>
2018-10-28 17:48:16 +0200

    add test for ordered list

--- test/org-drawer-list-test.el
+++ test/org-drawer-list-test.el
@@ -117,6 +117,45 @@
                 ,create
                 ,inside
                 )
+     (make-test ordered-list
+                ("{* Some heading"
+                 ":PROPERTIES:"
+                 ":ID: 8366A66A-2DE6-401B-AF7F-0C03C33EA3BB"
+                 ":END:"
+                 "<:RESOURCES:"
+                 "≤1. element1"
+                 "2. element2"
+                 "3. element3"
+                 "4. element4"
+                 "5. element5"
+                 "6. element6"
+                 "7. element7"
+                 "8. element8"
+                 "9. element9"
+                 "10. element10 is a long,"
+                 "    very long element"
+                 "11. element11"
+                 "12. element12"
+                 "≥:END:"
+                 ">}")
+                "RESOURCES"
+                ("element1"
+                 "element2"
+                 "element3"
+                 "element4"
+                 "element5"
+                 "element6"
+                 "element7"
+                 "element8"
+                 "element9"
+                 "element10 is a long, very long element"
+                 "element11"
+                 "element12")
+                ,drawer-name-case
+                ,point-location
+                ,create
+                ,inside
+                )
      (make-test multiple-headers
                 ("* Random first header"
                  ":PROPERTIES:"
